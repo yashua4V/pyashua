6fccc36f109a7506d23ff6137707d7f1356ecb20
linke <yashua@qq.com>
2019-05-24 12:33:20 +0800

    add

---
+++ test_os_fork.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+print('Process (%s) start ...' % os.getpid())
+pid = os.fork()
+if pid == 0:
+    print('i am child process (%s) and my parent is %s.' %(os.getpid(),os.getppid()))
+else:
+    print('i (%s) just created a child process (%s).' % (os.getpid(),pid))
